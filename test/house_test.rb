3f7307c4d4e3a66358f5465a97088694867f7da9
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2020-12-09 14:50:26 -0700

    Add tests for rooms_from_category and can_get_area. WIP.

--- test/house_test.rb
+++ test/house_test.rb
@@ -24,11 +24,36 @@ class HouseTest < Minitest::Test
     room_1 = Room.new(:bedroom, 10, '13')
     room_2 = Room.new(:bedroom, 11, '15')
     house.add_room(room_1)
-    assert_equal [room_1], house.rooms
+    house.add_room(room_2)
+    assert_equal [room_1, room_2], house.rooms
   end
 
   def test_above_market_average?
     house = House.new("$400000", "123 sugar lane")
     assert_equal false, house.above_market_average?
   end
+
+  def test_rooms_from_category
+    skip
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    assert_equal [room_1, room_2], house.rooms_from_category(:bedroom)
+  end
+
+  def test_it_can_get_area
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    assert_equal 1900, house.area
+  end
+
 end
